4c73c010cef7ae201d0a33fb3f97d40400a77655
mrmangoz <40361768+mrmangoz@users.noreply.github.com>
2020-04-05 19:05:47 +0200

    Add files via upload

--- symbols.py
+++ symbols.py
@@ -5,19 +5,13 @@ class Symbols:
     def add(self, symbol):
         self.symbols.append(symbol)
 
-    def check_loop(self, i, j):
-        for i in range(3):
-            for j in range(3):
-                for symbol in self.symbols:
-                    if symbol.pos == (i, j):
-                        return(True)
-                    else:
-                        return(False)
-
     def line_check(self, symbol):
         found = 0
-        if self.check_loop():
-            found += 1
+        for x in range(3):
+            for y in range(3):
+                for symbol in self.symbols:
+                    if symbol.pos == (x, y):
+                        found += 1
         #print(found)
         if found == 3:
             return("line found")
